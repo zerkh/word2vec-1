7f6acdd135415f592f5dfbe0b3eedd096bc33493
zerkh <mhzx20@163.com>
2015-11-20 16:18:21 +0800

    Update compute-accuracy.c

--- compute-accuracy.c
+++ compute-accuracy.c
@@ -12,6 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+dsadsadsadas
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
